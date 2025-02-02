78387e2511abdf867706d4c8dbb3194209d96c2e
AJTimePyro <ajtimepyro@gmail.com>
2025-02-02 16:59:17 +0530

    fixed some parser issue

--- pdfDataParser/pdfParser.py
+++ pdfDataParser/pdfParser.py
@@ -4,31 +4,21 @@ import os
 class PDFParser:
     __filePath : str | None
     __pdf_pointer : PdfReader | None
-    pdf_pages_list : List[PageObject]
+    pdf_pages_list : list[PageObject]
 
     def __init__(self, filePath = ''):
         if not filePath:
+            raise ValueError("File path cannot be empty.")
+        elif not os.path.isfile(filePath):
+            raise FileNotFoundError("File does not exist.")
+        else:
             self.__filePath = filePath
         self.__pdf_pointer = None
-    
-    @property
-    def filePath(self):
-        if self.filePath is None:
-            raise ValueError("No file path provided.")
-        return self.__filePath
-    
-    @filePath.setter
-    def filePath(self, value: str):
-        if not value:
-            raise ValueError("File path cannot be empty.")
-        elif os.path.isfile(value):
-            raise ValueError("File does not exist.")
-        self.__filePath = value
 
     def __get_pdf_pointer(self):
-        self.__pdf_pointer = PdfReader(self.filePath)
+        self.__pdf_pointer = PdfReader(self.__filePath)
     
-    def __parsing_pdf_pages(self, page_number: int):
+    def __parsing_pdf_pages(self):
         self.pdf_pages_list = self.__pdf_pointer.pages
     
     def parsePdf(self):
