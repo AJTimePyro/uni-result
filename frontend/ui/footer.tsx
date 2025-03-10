53ecd232508a825988a9ebfd64af12ddb4c9709c
10-abhi <abhishek81936@gmail.com>
2025-03-10 17:21:45 +0530

    adding some more comps

---
+++ frontend/ui/footer.tsx
@@ -0,0 +1,8 @@
+"use client"
+export default function Footer() {
+    return (
+      <footer className="bg-white dark:bg-gray-900 text-center p-4 shadow-md">
+        <p className="text-gray-600 dark:text-gray-300">&copy; 2025 Result Portal. All Rights Reserved.</p>
+      </footer>
+    );
+  }
