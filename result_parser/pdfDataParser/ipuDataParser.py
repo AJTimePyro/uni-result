d86831f1ad743067d2f565aa57fd44feeebf6a63
AJTimePyro <ajtimepyro@gmail.com>
2025-05-01 11:34:46 +0530

    removed unecessary thing

--- result_parser/pdfDataParser/ipuDataParser.py
+++ result_parser/pdfDataParser/ipuDataParser.py
@@ -461,7 +461,7 @@ class IPU_Result_Parser:
                 subject_id = self.__self_cleaning_subject_code(subject_id)
                 subject_id = standardize_subject_code(subject_id)
                 if self.__res_db.subject_id_code_map.get(subject_id, None) is None:
-                    parser_logger.warning(f"Subject ID not found in given subject page, raw data: {subject_id}, subject list: {self.__res_db.subject_id_code_map}, trying for database")
+                    parser_logger.warning(f"Subject ID not found in given subject page, raw data: {subject_id}, trying for database")
 
                     subject_id = self.__res_db.get_subject_id_by_code(student_n_subject_detail[subject_start_index])
                     if not subject_id:
